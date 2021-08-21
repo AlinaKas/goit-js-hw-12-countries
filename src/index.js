b8bfaf7ba6143e97595b968e1eedc8bb556448fc
Alina Kasianova <avkasyanova@gmail.com>
2021-08-21 11:31:18 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './sass/main.scss'
+
 import API from './js/fetchCountries'
 import getRefs from './js/getRefs'
+
 import countriesList from './templates/countriesList.hbs'
 import countryCardTpl from './templates/cardMarkup.hbs'
 
@@ -19,8 +21,8 @@ function onSearch(e) {
     e.preventDefault();
     const searchQuery = e.target.value;
     // const searchQuery = refs.input.value;
+    refs.container.innerHTML = '';
     if (!searchQuery) {
-        refs.container.innerHTML = '';
         return;
   }
     API.fetchCountries(searchQuery).then(renderCountryCard)
@@ -32,9 +34,15 @@ function renderCountryCard(country) {
     refs.container.innerHTML = markup;
     // refs.input.value = '';
 
-    if (country.status === 404 || country.length > 10) {
+    if (country.status === 404) {
         refs.container.innerHTML = '';
     onFetchError();
+    return;
+    }
+
+    if (country.length > 10) {
+        refs.container.innerHTML = '';
+    onFetchLengthError();
     return;
     }
 
@@ -47,11 +55,19 @@ function renderCountryCard(country) {
 
 function onFetchError() {
     error({
-        title: 'TRY AGAIN!',
-        text: 'Too many matches found.Please enter a more specific query!',
-        delay: 3000,
+        title: 'INCORRECT REQUEST',
+        text: 'Please enter a more specific query!',
+        delay: 2000,
   })
 };
+
+function onFetchLengthError() {
+    error({
+        title: 'Too many matches found',
+        text: 'Please enter a more specific query!',
+        delay: 2000,
+  })
+}
 
 
 
